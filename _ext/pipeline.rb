4aada3ca1e8c81269d68e8a95f7ab207e269e083
Jeff Mesnil <jmesnil@gmail.com>
2013-08-21 21:01:47 +0200

    add news/

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,9 @@
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Posts.new '/news', :posts
+  extension Awestruct::Extensions::Paginator.new :posts, '/news/index', :per_page => 2
   extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+
+  helper Awestruct::Extensions::Partial
 end
