fe245a4298e87230909b3b5caf7a595ca5bf9f8a
Jeff Mesnil <jmesnil@gmail.com>
2014-11-17 11:24:32 +0100

    display 20 articles per news pages, and the 3 most recent news on the home page

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,6 @@
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new '/news', :posts
-  extension Awestruct::Extensions::Paginator.new :posts, '/news/index', :per_page => 2
+  extension Awestruct::Extensions::Paginator.new :posts, '/news/index', :per_page => 20
   extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
